7b86f6dba2818b677598e3dacc52cc8d89a5294f
Ryuta Satoh <ryuta@local>
2021-09-25 20:40:24 +0900

    First commit

---
+++ symlinker.py
@@ -0,0 +1,87 @@
+import argparse
+import logging
+import os
+
+from filecmp import dircmp
+from typing import List
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src', '-s', required=True)
+    parser.add_argument('--dst', '-d', required=True)
+    parser.add_argument('--filter_ext', '-f', nargs='*', default=['mov', 'm4a', 'mp4', '3gp'])
+
+    return parser.parse_args()
+
+
+def check_srconly_and_symlink(src: str, dst: str, dc: dircmp, filter_ext: List[str]) -> None:
+    srconly = dc.left_only
+
+    for p in srconly:
+        splitext = os.path.splitext(p)
+
+        if len(splitext) > 1:
+            if len(splitext[1]) > 2 and splitext[1][1:].lower() not in filter_ext:
+                # only if a path ...
+                # * has an extension
+                # * an extension not in filter list
+
+                os.symlink(
+                    os.path.join(src, p),
+                    os.path.join(dst, p)
+                )
+
+        else:
+            # no extensions, probably a directory
+            new_src_dir = os.path.join(src, p)
+            if os.path.isdir(new_src_dir):
+                # it is directory, mkdir it and go into it
+                new_dst_dir = os.path.join(dst, p)
+                os.mkdir(new_dst_dir)
+
+                # For simplicity, use same code even when
+                # clearly comparison is not needed
+                subdc = dircmp(new_src_dir, new_dst_dir)
+                check_srconly_and_symlink(
+                    new_src_dir,
+                    new_dst_dir,
+                    subdc,
+                    filter_ext
+                )
+
+    # check sub directories
+    for subdc in dc.subdirs.values():
+        check_srconly_and_symlink(
+            os.path.join(src, subdc.left),
+            os.path.join(dst, subdc.right),
+            subdc,
+            filter_ext
+        )
+
+
+def main() -> None:
+    args = parse_args()
+
+    if not os.path.exists(args.src):
+        logging.error(f'source path not found ({args.src})')
+        exit(-1)
+    if not os.path.isdir(args.src):
+        logging.error(f'source path is not a directory ({args.src})')
+        exit(-1)
+
+    if not os.path.exists(args.dst):
+        logging.error(f'destination path not found ({args.dst})')
+        exit(-1)
+    if not os.path.isdir(args.dst):
+        logging.error(f'destination path is not a directory ({args.dst})')
+        exit(-1)
+
+    dc = dircmp(args.src, args.dst)
+    check_srconly_and_symlink(
+        args.src, args.dst, dc, args.filter_ext
+    )
+
+
+if __name__ == '__main__':
+    main()
